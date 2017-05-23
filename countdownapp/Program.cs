b008e5cdba3d5734a10019338e7b606b74b55572
Carmen Long <carmen.long@hobbylobby.com>
2017-05-23 13:59:44 -0500

    ww times in hours && minutes

---
+++ countdownapp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace countdownapp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie_data = new Movie();
+            try
+            {
+                movie_data = JsonConvert.DeserializeObject<Movie>(File.ReadAllText(@"C:\_Git\countdownapp\countdownapp\config.json"));
+            }
+            catch
+            {
+                movie_data.Event = "Wonder Woman";
+                movie_data.Date = "06-02-2017";
+            }
+            finally
+            {
+                string MovieTitle = movie_data.Event;
+                string HowLongDoIHaveToWait = HoursRemaining(movie_data.Date);
+                Console.WriteLine(MovieTitle + HowLongDoIHaveToWait);
+                Console.ReadKey();
+            }
+        }
+
+        public static string HoursRemaining(string day)
+        {
+            DateTime dt = Convert.ToDateTime(day);
+            DateTime now = DateTime.Now;
+            var diffInMinutes = (dt - now).TotalMinutes;
+            var diffInHours = diffInMinutes / 60;
+            var appendMinutes = diffInMinutes % 60;
+            string hours = " will arrive in " + Math.Truncate(diffInHours) + " hours ";
+            string minutes = appendMinutes > 0 ? "and " + Math.Truncate(appendMinutes).ToString() + " minutes" : "";
+            return hours + minutes;
+        }
+    }
+}
